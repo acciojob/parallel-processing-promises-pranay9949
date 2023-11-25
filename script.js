2499908a57a4e5e225e1cd1cfef793318f1e3b9f
pranay9949 <139853595+pranay9949@users.noreply.github.com>
2023-11-25 11:16:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,3 +7,26 @@ const images = [
   { url: "https://picsum.photos/id/238/200/300" },
   { url: "https://picsum.photos/id/239/200/300" },
 ];
+
+function loadImage(url) {
+  return new Promise((resolve, reject) => {
+    const img = new Image();
+    img.onload = () => resolve(img);
+    img.onerror = () => reject(new Error(`Failed to load image: ${url}`));
+    img.src = url;
+  });
+}
+
+let imagePromises = images.map(img => loadImage(img.url));
+
+Promise.all(imagePromises)
+  .then(images => {
+    console.log("All images loaded successfully:", images);
+    // You can use the loaded images here, for example, append them to the output element
+    images.forEach(img => {
+      output.appendChild(img);
+    });
+  })
+  .catch(error => {
+    console.error("Error loading one or more images:", error);
+  });
